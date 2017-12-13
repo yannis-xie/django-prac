57ed0aafaa9d7378cb77959f832ba35ff1c1c1a0
yannis-xie <yxie0822@126.com>
2017-12-13 16:58:16 +0800

    login & header page update

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,11 @@
 
 from django.conf.urls import url
 from . import views
-
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name = 'user_login'),
+    # url(r'^login/$', views.user_login, name = 'user_login'),
+    url(r'^login/$', auth_views.login, name = 'user_login'),
+    url(r'^new_login/$', auth_views.login, {"template_name": "account/login.html"})
 ]
