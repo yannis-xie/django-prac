bdec0e846505c26b336f2e8160eab0fa38890da8
yannis-xie <yxie0822@126.com>
2017-12-24 22:37:01 +0800

    profile upload from home

--- account/views.py
+++ account/views.py
@@ -3,10 +3,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, RegistrationForm, UserProfileForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm, UserForm, UserInfoForm
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile, UserInfo
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -54,3 +55,42 @@ def myself(request):
     userprofile = UserProfile.objects.get(user = user)
     userinfo = UserInfo.objects.get(user = user)
     return render(request, 'account/myself.html', {'user':user, 'userinfo':userinfo, 'userprofile':userprofile})
+
+@login_required(login_url = '/account/login')
+def myself_edit(request):
+    user = User.objects.get(username=request.user.username)
+    userprofile = UserProfile.objects.get(user=user)
+    userinfo = UserInfo.objects.get(user=user)
+    # return render(request, 'account/myself.html', {'user':user, 'userinfo':userinfo, 'userprofile':userprofile})
+    if request.method == "POST":
+        user_form = UserForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        userinfo_form = UserInfoForm(request.POST)
+        if user_form.is_valid() * userprofile_form.is_valid() * userinfo_form.is_valid():
+            user_cd = user_form.cleaned_data
+            userprofile_cd = userprofile_form.cleaned_data
+            userinfo_cd = userinfo_form.cleaned_data
+            print(user_cd['email'])
+            user.email = user_cd['email']
+            userprofile.birth = userprofile_cd['birth']
+            userprofile.phone = userprofile_cd['phone']
+            userinfo.school = userinfo_cd['school']
+            userinfo.company = userinfo_cd['company']
+            userinfo.profession = userinfo_cd['profession']
+            userinfo.address = userinfo_cd['address']
+            userinfo.aboutme = userinfo_cd['aboutme']
+            user.save()
+            userprofile.save()
+            userinfo.save()
+        return HttpResponseRedirect('/account/my-information/')
+    else:
+        user_form = UserForm(instance=request.user)
+        userprofile_form = UserProfileForm(initial={"birth": userprofile.birth, "phone": userprofile.phone})
+        userinfo_form = UserInfoForm(
+            initial={"school": userinfo.school, "company": userinfo.company, "profession": userinfo.profession,
+                     "address": userinfo.address, "aboutme": userinfo.aboutme})
+        return render(request, "account/myself_edit.html",
+                      {"user_form": user_form, "userprofile_form": userprofile_form, "userinfo_form": userinfo_form})
+
+def my_image(request):
+    return render(request, 'account/imagecrop.html')
