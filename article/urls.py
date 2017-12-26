9feec6073553dcb2db0fd847f0e88a6ce4974f15
yannis-xie <yxie0822@126.com>
2017-12-26 14:48:18 +0800

    add article implement

--- article/urls.py
+++ article/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^article-column', views.article_column, namespace = 'article_column'),
+    url(r'^article-column', views.article_column, name = 'article_column'),
 ]
