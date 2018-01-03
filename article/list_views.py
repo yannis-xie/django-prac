34542d4bcab5e81cfe26e093af19ff59944a57b4
yannis-xie <yxie0822@126.com>
2018-01-03 22:34:42 +0800

    add article detial

--- article/list_views.py
+++ article/list_views.py
@@ -1,8 +1,9 @@
 #-*-coding:utf8-*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import ArticleColumn, ArticlePost
+
 
 def article_titles(request):
     articles_title = ArticlePost.objects.all()
@@ -18,3 +19,8 @@ def article_titles(request):
         current_page = paginator.page(paginator.num_pages)
         articles = current_page.object_list
     return render(request, 'article/list/article_titles.html', {'articles':articles, 'page':current_page})
+
+
+def article_detail(request, id, slug):
+    article = get_object_or_404(ArticlePost, id = id, slug = slug)
+    return render(request, 'article/list/article_detail.html', {'article':article})
