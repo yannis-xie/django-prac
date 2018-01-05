3135d874be50e3adb1149f85d55caa5f56d46e80
yannis-xie <yxie0822@126.com>
2018-01-05 22:05:19 +0800

    comment added

--- article/list_views.py
+++ article/list_views.py
@@ -2,11 +2,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import ArticleColumn, ArticlePost
+from .models import ArticleColumn, ArticlePost, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+import redis
+from django.conf import settings
+from .forms import CommentForm
+
+r = redis.StrictRedis(host = settings.REDIS_HOST, port = settings.REDIS_PORT, db = settings.REDIS_DB)
 
 
 def article_titles(request, username = None):
@@ -38,7 +43,25 @@ def article_titles(request, username = None):
 
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id = id, slug = slug)
-    return render(request, 'article/list/article_detail.html', {'article':article})
+    total_views = r.incr('article:{}:views'.format(article.id))
+    r.zincrby('article_ranking', article.id, 1)
+
+    article_ranking = r.zrange('article_ranking', 0, -1, desc = True)[:10]
+    article_ranking_ids = [int(id) for id in article_ranking]
+    most_viewed = list(ArticlePost.objects.filter(id__in = article_ranking_ids))
+    most_viewed.sort(key = lambda x: article_ranking_ids.index(x.id))
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit = False)
+            new_comment.article = article
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'article/list/article_detail.html', {'article':article, 'total_views':total_views,
+                                                                'most_viewed':most_viewed, 'comment_form':comment_form})
 
 @csrf_exempt
 @require_POST
--- article/models.py
+++ article/models.py
@@ -40,3 +40,15 @@ class ArticlePost(models.Model):
 
     def get_url_path(self):
         return reverse('article:list_article_detail', args = [self.id, self.slug])
+
+class Comment(models.Model):
+    article = models.ForeignKey(ArticlePost, related_name = 'comments')
+    commentator = models.CharField(max_length = 20)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return "Comment by {0} on {1}".format(self.commentator.username, self.article)
