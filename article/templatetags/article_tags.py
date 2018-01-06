0b0f6ec6fe01efda833c287875bec4a6c78630d3
yannis-xie <yxie0822@126.com>
2018-01-06 22:30:38 +0800

    total article count added

---
+++ article/templatetags/article_tags.py
@@ -0,0 +1,11 @@
+#-*-coding:utf8-*-
+
+from django import template
+
+register = template.Library()
+
+from article.models import ArticlePost
+
+@register.simple_tag
+def total_articles():
+    return ArticlePost.objects.count()
