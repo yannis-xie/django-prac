ca31e63896fa1281a6737053652db4963ac4a1bb
Xie <Yan4.Xie@sonymobile.com>
2018-01-08 17:25:31 +0800

    author articles

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -9,3 +9,7 @@ from article.models import ArticlePost
 @register.simple_tag
 def total_articles():
     return ArticlePost.objects.count()
+
+@register.simple_tag
+def author_total_articles(user):
+    return user.article.count()
